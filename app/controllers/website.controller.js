69e9ae61b2566fb2cf69ad3f1d4a7bfa7522d84c
Bart Sevenois <bart.sevenois@EfficyLounge.local>
2019-01-31 16:51:55 +0100

    Language imort I18N

--- app/controllers/website.controller.js
+++ app/controllers/website.controller.js
@@ -1,3 +1,22 @@
 exports.renderHome = (req, res) => {
-    res.render('index');
+    var language;
+    var defLang = req.headers["accept-language"].substring(0, 2);
+    if (defLang == "nl") {
+        language = "nl";
+    } else if (defLang == "fr") {
+        language = "fr";
+    } else if (defLang == "en") {
+        language = "en";
+    }
+    if (req.query.lang != undefined){
+        req.session.lang = req.query.lang;
+        res.setLocale(req.query.lang);
+        language = req.query.lang;
+    } else if(req.session.lang != undefined){
+        res.setLocale(req.session.lang);
+        language = req.query.lang;
+    }
+    res.render('index', {
+        lang: language
+    });
 }
--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 var express = require('express');
+var i18n = require('./app/config/i18n');
+var expressSession = require('express-session');
 
 var app = express();
 app.use(express.static(__dirname + '/app/public'));
+app.use(i18n);
+app.use(expressSession({secret: 'max', saveUninitialized: false, resave: false}));
+
 app.set('views', __dirname + '/app/views');
 app.set('view engine', 'ejs');
 
